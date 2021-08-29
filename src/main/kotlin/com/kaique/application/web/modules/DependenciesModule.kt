a089c4ac0c8b3c25db0e12f48e782c76aa38a40a
kaiqkt <kaique..gomes@bandtec.com.br>
2021-08-29 01:10:40 -0300

    changed some packages

---
+++ src/main/kotlin/com/kaique/application/web/modules/DependenciesModule.kt
@@ -0,0 +1,40 @@
+package com.kaique.application.web.modules
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.kaique.application.configs.AuthConfig
+import com.kaique.application.configs.mongoDatabase
+import com.kaique.resources.kafka.EventConsumer
+import com.kaique.resources.kafka.EventProducer
+import com.mongodb.ConnectionString
+import org.koin.dsl.module.module
+
+val dependenciesModule = module {
+    single {
+        AuthConfig(getProperty("SERVICE_TOKEN"))
+    }
+
+    single {
+        mongoDatabase(
+            connectionString = ConnectionString("mongodb://${getProperty<String>("MONGODB_CONNECTION_STRING")}"),
+            username = getProperty("MONGODB_USER"),
+            password = getProperty("MONGODB_PASSWORD"),
+            databaseName = getProperty("MONGODB_DATABASE")
+        )
+    }
+
+    single {
+        EventConsumer(
+            getProperty("KAFKA_URL"),
+            getProperty("KAFKA_TOPIC"),
+            ObjectMapper()
+        )
+    }
+
+    single {
+        EventProducer(
+            getProperty("KAFKA_URL"),
+            getProperty("KAFKA_TOPIC"),
+            ObjectMapper()
+        )
+    }
+}
